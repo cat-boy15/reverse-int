dc361d2efa82d7410522e3cc004a6ef3e44a13d6
cat-boy15 <96006975+cat-boy15@users.noreply.github.com>
2022-01-11 17:59:38 +1000

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let yoString = Math.abs(n).toString();
+  let yoArray = yoString.split('');
+  let result = yoArray.reverse().join('');
+
+  return result
 }
